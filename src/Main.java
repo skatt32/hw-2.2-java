70279d4aee87133ca249e2b2dd00b3dbd2b62de9
Neto test <test@neto.com>
2023-11-15 18:58:24 +0400

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int bonus = 50; // бонусные рубли
+
+        int onTheAccount = 250; // начальный счет
+        int replenishment = 300; // бонусов нет
+        int replenishment1 = 450; // бонусы начисляются
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+        if (replenishment >400)
+            replenishment= replenishment + bonus;
+        if (replenishment1 >400)
+            replenishment1= replenishment1 + bonus;
+
+        System.out.println("Пополненный счет без начисленных бонусов");
+        System.out.println(onTheAccount + replenishment);
+        System.out.println("Пополненный счет с начисленными бонусами");
+        System.out.println(onTheAccount + replenishment1);
+    }
 }
